ced8af981f6b849ad414babf6294fc91d6b5a532
c <cashignitro@gmail.com>
2024-10-01 18:10:07 +1000

    to.fish update

--- .config/fish/functions/to.fish
+++ .config/fish/functions/to.fish
@@ -1,7 +1,19 @@
 function to
     if test (count $argv) -gt 0
         set search_term $argv[1]
-        cd $(ls -d /media/chdd2/dev/ /media/chdd2/code/* ~/.dotfiles | fzf --select-1 --query="$search_term")
+        set available $(ls -d /media/chdd2/dev/ /media/chdd2/code/* ~/.dotfiles)
+
+        # see if selection is EMPTY
+        if test -z "$available"
+            exit
+        end
+        
+        set selection $(printf "%s\n" $available | fzf --select-1 --exit-0 --query="$search_term")
+
+        # see if selection is NOT EMPTY
+        if test -n "$selection"
+            cd $selection
+        end
     else
         cd $(ls -d /media/chdd2/dev/ /media/chdd2/code/* ~/.dotfiles | fzf)
     end
