7be37cd7c1c34c29b74bb9cabeb027e85a9fdb34
cash-i1 <117874913+cash-i1@users.noreply.github.com>
2025-07-01 16:13:44 +1000

    disabled some neovim plugins + rot

--- .config/nvim/lua/plugins/colo.lua
+++ .config/nvim/lua/plugins/colo.lua
@@ -2,6 +2,8 @@ return {
     { "Mofiqul/vscode.nvim" },
     { "oonamo/ef-themes.nvim" },
     { "rebelot/kanagawa.nvim" },
+    -- { "squarefrog/tomorrow-night.vim" },
+    -- { "chriskempson/vim-tomorrow-theme" },
     -- { "projekt0n/github-nvim-theme" },
     priority = 1000
 }
